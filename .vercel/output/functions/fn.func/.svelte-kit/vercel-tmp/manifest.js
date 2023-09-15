663f2ba5e40649d3d4b4436cedf30d452b67c009
pvk05 <philip.v.knudsen@gmail.com>
2023-09-15 10:53:33 +0200

    changed select

--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.d062ddd0.js","app":"_app/immutable/entry/app.cf087469.js","imports":["_app/immutable/entry/start.d062ddd0.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/singletons.38b61997.js","_app/immutable/entry/app.cf087469.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.561f35bb.js","app":"_app/immutable/entry/app.b9dce3d0.js","imports":["_app/immutable/entry/start.561f35bb.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/singletons.e09d678b.js","_app/immutable/entry/app.b9dce3d0.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
