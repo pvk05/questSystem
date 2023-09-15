36e8da2d16196e5828e63c66936b67c351aa52ed
pvk05 <philip.v.knudsen@gmail.com>
2023-09-15 12:47:11 +0200

    fixed multiple select

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.19d1cc3d.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js","_app/immutable/chunks/singletons.e09d678b.js"];
+export const imports = ["_app/immutable/nodes/1.2daec811.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js","_app/immutable/chunks/singletons.856940a6.js"];
 export const stylesheets = [];
 export const fonts = [];
--- .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
+++ .vercel/output/functions/fn.func/.svelte-kit/vercel-tmp/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.561f35bb.js","app":"_app/immutable/entry/app.b9dce3d0.js","imports":["_app/immutable/entry/start.561f35bb.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/singletons.e09d678b.js","_app/immutable/entry/app.b9dce3d0.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.1e5df307.js","app":"_app/immutable/entry/app.38f8e10e.js","imports":["_app/immutable/entry/start.1e5df307.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/singletons.856940a6.js","_app/immutable/entry/app.38f8e10e.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('../output/server/nodes/0.js')),
 			__memo(() => import('../output/server/nodes/1.js'))
