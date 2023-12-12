a7c271a7bccbfc07857f4ff8e35848e80c100bfe
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 12:14:36 +0100

    Remove unused code for quest form

--- src/routes/questForm.ts
+++
@@ -1,8 +0,0 @@
-/*import db from "$lib/db"
-export async function GET() {
-  const biomes = await db.collection('biomes').find().toArray()
-  return {
-    status: 200,
-    body: { biomes }
-  }
-}*/
