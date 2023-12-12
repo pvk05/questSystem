5563e93e3bd3a1fe0af6b201dff7ac13e21d3e14
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 12:10:55 +0100

    Update Supabase client configuration

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public'
 
-export const supabase = createClient('https://acuallorrbcusceblnda.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjdWFsbG9ycmJjdXNjZWJsbmRhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTM4NTY4MzMsImV4cCI6MjAwOTQzMjgzM30.iXjlE-OYmoqrFBoxmY_vOFf4lpWQVpJWVS2-hV5tvo4')
+export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
