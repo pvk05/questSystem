565f80ca0ec457bb595e1ed05f7da8fc88d2f184
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 11:26:57 +0100

    Add Netlify configuration and Svelte Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -10,7 +10,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			edge: false,
+
+			split: false
+		}),
 
 		// Add preprocess to handle TypeScript and other preprocessing
 		
