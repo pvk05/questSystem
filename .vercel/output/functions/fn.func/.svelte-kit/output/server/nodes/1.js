194f47a6f29edc06240aec0624e79e319625d012
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 12:18:20 +0100

    Remove unused files and dependencies

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.f26c8321.js","_app/immutable/chunks/scheduler.b95eede2.js","_app/immutable/chunks/index.9d996ac2.js","_app/immutable/chunks/singletons.12f3bdc0.js"];
-export const stylesheets = [];
-export const fonts = [];
