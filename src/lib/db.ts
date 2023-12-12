47cd9b35df16723537875e608c5b289cb03211d2
pvk05 <philip.v.knudsen@gmail.com>
2023-12-12 11:51:40 +0100

    Remove unused MongoDB connection code

--- src/lib/db.ts
+++
@@ -1,5 +0,0 @@
-import { MongoClient } from "mongodb"
-import {DB_URI} from '$env/static/private'
-const client = new MongoClient(DB_URI)
-await client.connect()
-export default client.db('data') // select database
